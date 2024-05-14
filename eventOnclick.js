0d43cde7a3ee1ce0a9f911a2be541a29cd7da4f7
dvallon906 <d.vallon@icloud.com>
2024-05-14 19:25:26 +0200

    Ajout de eventOnclic

---
+++ eventOnclick.js
@@ -0,0 +1,13 @@
+
+const dateTimeNow = new Date()
+
+const dateTimeFormatter = new Intl.DateTimeFormat('fr-FR', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+    timeZone: 'Europe/Paris'
+})
+
+
+function myFunction(){
+    alert(dateTimeFormatter.format(dateTimeNow));
+} 
